d1a551f3e8fc4280e9951448e1085dcc77f7f87f
yedegesong <277527478@qq.com>
2017-04-08 23:31:06 +0800

    更改案例

---
+++ src/controller/ExampleController.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>Hello,Word</div>
+</template>
+
+<script lang = "ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  @Component
+  export default class ExampleController extends Vue {
+    msg = 123
+  
+    // use prop values for initial data
+    helloMsg = 'Hello, ' + this.propMessage
+    
+    beforeCreate(){
+      console.log('实例已经创建,msg变量还未渲染到模板')
+    }
+
+    created(){
+      console.log('实例已经创建,msg变量还未渲染到模板')
+    }
+    // lifecycle hook
+    mounted() {
+      console.log('已经挂载到模板上:msg变量渲染到模板')
+      //this.greet()
+    }
+    
+    updated(){
+      console.log('实例更新啦')   
+    }
+
+    destroyed(){
+       console.log('销毁啦')
+    }
+
+    // computed
+    get computedMsg() {
+      return 'computed ' + this.msg
+    }
+  
+    // method
+    greet() {
+      alert('greeting: ' + this.msg)
+    }
+  }
+</script>
--- src/controller/home/HomeController.vue
+++ src/controller/home/HomeController.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <div data-reactroot=""
-         class="view container container-fill container-column"
-         id="sk-container">
+    <div data-reactroot="" class="view container container-fill container-column">
       <div class="views">
         <div class="view">
           <div class="container-scrollable view-bg">
@@ -11,9 +9,7 @@
                 <div class="ui-col ui-member-img"><img src="http://wx.qlogo.cn/mmopen/ajNVdqHZLLA3qFZxiaSxTia4eXpk73lE6GoBib73uV05WRn9e3TOYgeD3FMRojPdT04jG5bfMiclEtEiaq9UUeHxN5Q/0"></div>
                 <div class="ui-col ui-col-1 ui-grid-flex-info user-infor">
                   <h1></h1>
-                  <div><span>{{msg}}</span>
-                    <!-- react-text: 13 -->(次)
-                    <!-- /react-text -->
+                  <div><span>{{msg}}</span> (次)
                   </div>
                 </div>
               </div>
@@ -23,31 +19,18 @@
               <div class="pn-tomanth">
                 <div class="ui-row-flex ui-grid-flex-list pn-munber">
                   <div class="ui-col ui-col-1 ">
-                    <!-- react-text: 20 -->本月推广
-                    <!-- /react-text --><span>(次)</span></div>
+                    本月推广
+                    <span>(次)</span></div>
                   <div class="ui-col ui-col-1 ui-text-right pn-tot">
                     <div>2</div><span>数据结算为前一天</span></div>
                 </div>
               </div>
             </div>
             <div class="pn-list">
-              <div class="ui-row-flex ui-grid-flex ui-border-b ui-arrowlink">
-                <div class="ui-col ui-col-1 ui-grid-flex-info dark-grey">推广记录</div>
-              </div>
-              <div class="ui-row-flex ui-grid-flex ui-border-b ui-arrowlink">
-                <div class="ui-col ui-col-1 ui-grid-flex-info dark-grey">推广新用户</div>
-              </div>
-              <div class="ui-row-flex ui-grid-flex ui-border-b ui-arrowlink">
-                <div class="ui-col ui-col-1 ui-grid-flex-info dark-grey">添加成员</div>
-              </div>
-              <div class="ui-row-flex ui-grid-flex ui-border-b ui-arrowlink">
-                <div class="ui-col ui-col-1 ui-grid-flex-info dark-grey">我的成员</div>
-              </div>
+              <LinkLi v-for="item in menuLists">{{item.info}}</LinkLi>
             </div>
             <div>
-              <button display="block"
-                      type="danger"
-                      class="ui-btn ui-btn-danger ui-btn-block">退出</button>
+              <button display="block" type="danger" class="ui-btn ui-btn-danger ui-btn-block">退出</button>
             </div>
           </div>
         </div>
@@ -55,22 +38,64 @@
     </div>
   </div>
 </template>
+
 <script lang = "ts">
-    /*export default {
-        data () {
-            return {
-                modeType: "vertical",
-                modal1: false,
-                spanLeft: 5,
-                spanRight: 19,
-                logoIsDisplay: false
-            }
-        }
-    }*/
-    import Vue from 'vue';
-    import Component from 'vue-class-component'
-    @Component
-    export default class HomeController extends Vue {
-      msg: number = 123456
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import LinkLi from './LinkLi.vue';
+  @Component({
+      name: 'HomeController',
+      components:{LinkLi}
     }
-</script> 
+  )
+  export default class HomeController extends Vue {
+  
+    menuLists = [{
+        info: '推广记录',
+        router: `/my/promotionRecord`
+      },
+      {
+        info: '推广新用户',
+        router: `/my/toPromote`
+      },
+      {
+        info: '添加成员',
+        router: `/my/addMembers`
+      },
+      {
+        info: '我的成员',
+        router: `/my/members/${this.user_id}`
+      }
+    ]
+  
+    beforeCreate() {
+      console.log('实例已经创建,msg变量还未渲染到模板')
+    }
+  
+    created() {
+      console.log('实例已经创建,msg变量还未渲染到模板');
+    }
+    // lifecycle hook
+    mounted() {
+      console.log('已经挂载到模板上:msg变量渲染到模板');
+    }
+  
+    updated() {
+      console.log('实例更新啦')
+    }
+  
+    destroyed() {
+      console.log('销毁啦')
+    }
+  
+    // computed
+    get computedMsg() {
+      return 'computed ' + this.msg
+    }
+  
+    // method
+    greet() {
+      alert('greeting: ' + this.msg)
+    }
+  }
+</script>
